80506a7666819565d62a08fe94b9b3dd80cd04ac
Gabriel R.P <0358929@gmail.com>
2025-04-25 11:22:43 -0400

    Initial sharing of project

---
+++ Athlete.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Athlete here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Athlete
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    //+ listActivitys : ArrayList
+    //+ burntCalories : Int
+    //+ getTotalDistance : Int
+    /**
+     * Constructor for objects of class Athlete
+     */
+    public Athlete()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
